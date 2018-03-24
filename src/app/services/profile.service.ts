d0693c3b4db4732942ac3baf34d1a6665df4a5dd
olamijinadebayo <consultolamijin@gmail.com>
2018-03-24 23:31:42 +0300

    this will add the id and key for the github api

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http;
-'
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProfileService {
+  private username: string;
+  private clientid = '69b0d23e02ad69fd8cb3';
+  private clientsgiercret = '1c53a114238cca3f4528eafa5069fa5bbfad9ad6';
 
-  constructor() { }
+  constructor(private http: Http, ) { }
 
 }
