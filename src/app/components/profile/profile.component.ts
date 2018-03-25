c204139546ac91d4a6216465e9dc24f012abed3f
olamijinadebayo <consultolamijin@gmail.com>
2018-03-25 18:16:53 +0300

    this will add the function to fetch repo data from the api

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -7,11 +7,17 @@ import { ProfileService } from '../../services/profile.service';
 })
 export class ProfileComponent implements OnInit {
   private profile: any[];
+  private repos: any[];
   constructor(private profileService: ProfileService) {
     this.profileService.getProfileInfo().subscribe(profile => {
       console.log(profile);
       this.profile = profile;
     });
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    });
+
   }
 
   ngOnInit() {
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -16,5 +16,8 @@ export class ProfileService {
     return this.http.get('https://api.github.com/users/' + this.username + '?client_id = ' + this.clientid + '&client_secret=' + this.clientsecret)
       .map(res => res.json());
   }
-
+  getProfileRepos() {
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id = ' + this.clientid + '&client_secret=' + this.clientsecret)
+      .map(res => res.json());
+  }
 }
